a3065f3d3628ba882f16cf531ec440c1fd35f445
ArchanaHarikrishnan <archana18harikrishnan@gmail.com>
2024-10-17 13:15:03 +0530

    Add project files.

---
+++ NullReferenceEx/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace NullReferenceEx
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = null;
+
+            try
+            {
+                Console.WriteLine(student.Name);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
+
+
+
